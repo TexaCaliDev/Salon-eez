1a316b6d180a9c964ba0f6688610583f9ec6bef7
benjmain roth <benjaminsavanne@gmail.com>
2021-01-05 08:09:32 -0600

    create user model

---
+++ migrations/20210105133720-create-user.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      company_name: {
+        type: Sequelize.STRING
+      },
+      company_email: {
+        type: Sequelize.STRING
+      },
+      company_password_digest: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users');
+  }
+};
