091181cb63db7e96ecc971c398ec17b9549579a7
benjmain roth <benjaminsavanne@gmail.com>
2021-01-05 20:02:09 -0600

    create equipment router

---
+++ routes/EquipmentRouter.js
@@ -0,0 +1,9 @@
+const Router = require('express').Router()
+const controller = require('../controllers/equipmentController')
+
+Router.get('/view/:equipment_id', controller.GetEquipmentDetails)
+Router.post('/create/:stylist_id,', controller.CreateEquipment)
+Router.put('/:equipment_id',controller.UpdateEquipment)
+Router.delete('/delete/:equipment_id', controller.DeleteEquipment)
+
+module.exports = Router
