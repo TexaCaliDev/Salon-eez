fb85929c655ea9474970e7b8a915a7521fa9f539
benjmain roth <benjaminsavanne@gmail.com>
2021-01-08 15:21:18 -0600

    move more files around

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <div>
+      <p>oh fuck yea this works</p>
+    </div>
+    </div>
+  );
+}
+
+export default App;
